0e7bdd2d5038a95fc7c2e193082d88d1b11981a9
lewnardu <lewnardu@yahoo.com.br>
2021-07-16 19:50:52 -0300

    Att icons nas templates de listagem

--- rhsispen/namp/urls.py
+++ rhsispen/namp/urls.py
@@ -9,31 +9,34 @@ app_name = 'namp'
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	#Tela do Operador
+	#path('equipe_operador_change_list/', views.equipe_operador_change_list, name='equipe_operador_change_list'),
+	#path('equipe_operador_att_form/<int:id_equipe>/', views.equipe_operador_att_form, name='equipe_operador_att_form'),
+	#path('equipe_delete/<int:id_equipe>/delete', views.EquipeDeleteView, name='equipe_delete'),
 
 	#Tela do GESTOR
 	path('admin_afastamento/', views.admin_afastamento, name='admin_afastamento'),
 	path('admin_servidor/', views.admin_servidor, name='admin_servidor'), #falta fazer
-	path('admin_setores/', views.admin_setores, name='admin_setores'), #falta fazer
-	path('admin_unidades/', views.admin_unidades, name='admin_unidades'), 
+	path('admin_unidades/', views.admin_unidades, name='admin_unidades'), #falta fazer
 	path('admin_historico/', views.admin_unidades, name='admin_historico'), #falta fazer
 	path('admin_add_noturno/', views.admin_add_noturno, name='admin_add_noturno'), #falta fazer
 	path('admin_servidores/', views.admin_servidores, name='admin_servidores'), #falta fazer
-	path('admin_escalas_frequencias/', views.admin_escalas_frequencias, name='admin_escalas_frequencias'),#falta fazer
+	path('admin_escalas_frequencias/', views.admin_escalas_frequencias, name='admin_escalas_frequencias'), #falta fazer
 
 	#Tela do OPERADOR
-	path('equipes/<int:id_equipe>/editar/', views.equipe_att, name='equipe_att'),
-	path('equipes/cadastrar/', views.equipe_criar, name='equipe_criar'),
-	path('equipes/', views.equipe_list, name='equipe_list'),
+	path('equipe_att/<int:id_equipe>/', views.equipe_att, name='equipe_att'),
+	path('equipe_criar/', views.equipe_criar, name='equipe_criar'),
+	path('equipe_list/', views.equipe_list, name='equipe_list'),
 	path('equipe_delete/<int:id_equipe>/delete', views.equipe_delete, name='equipe_delete'),
-	path('servidor_mov/', views.servidor_mov, name='servidor_mov'), 
+	path('servidor_mov/', views.servidor_mov, name='servidor_mov'), #falta fazer
 	path('servidor_list/', views.servidor_list, name='servidor_list'), #falta fazer
 	path('escala_operador_list/', views.escala_operador_list, name='escala_operador_list'),
 	path('jornadas_operador/', views.jornadas_operador, name='jornadas_operador'),
 	path('frequencia_operador_list/', views.frequencia_operador_list, name='frequencia_operador_list'),
-	path('operador_afastamentos/', views.operador_afastamentos, name='operador_afastamentos'), 	
+	path('operador_afastamentos/', views.operador_afastamentos, name='operador_afastamentos'), #falta fazer	
 
 	#Tela do SERVIDOR
-	path('servidores/<int:id_matricula>/editar/', views.servidor_att, name='servidor_att'),
+	path('servidor_att/<int:id_matricula>/', views.servidor_att, name='servidor_att'),
 	path('servidor_escala/', views.servidor_escala, name='servidor_escala'), #falta fazer
 	path('servidor_hist/', views.servidor_hist, name='servidor_hist'), #falta fazer
 
